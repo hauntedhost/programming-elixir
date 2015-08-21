6c584d1be6177be6c113e9fcdf567ceeadd00b72
Sean Omlor <somlor@eml.cc>
2015-08-21 18:05:29 -0400

    center list of strings

--- my_string.exs
+++ my_string.exs
@@ -1,5 +1,5 @@
 defmodule MyString do
-  import Enum, only: [map: 2]
+  import Enum, only: [join: 2, map: 2, max_by: 2, sort: 2]
   import String, only: [to_atom: 1, to_integer: 1]
 
   def calculate(expression)
@@ -7,11 +7,36 @@ defmodule MyString do
     do: calculate(to_string(expression))
 
   def calculate(expression) do
-    [num1, operator, num2] = _parse(expression)
+    [num1, operator, num2] = parse_expression(expression)
     apply(Kernel, to_atom(operator), map([num1, num2], &to_integer/1))
   end
 
-  defp _parse(expression) do
+  def center(words) do
+    words
+    |> sort_by_length
+    |> map_padding
+    |> join("\n")
+  end
+
+  def longest(words), do: max_by(words, &(String.length(&1)))
+
+  def sort_by_length(words), do: sort(words, &(length_asc(&1, &2)))
+
+  # private
+
+  defp length_asc(a, b), do: String.length(a) < String.length(b)
+
+  defp map_padding(words) do
+    max_length = longest(words) |> String.length
+    words |> Enum.map &(String.rjust(&1, padding(&1, max_length)))
+  end
+
+  defp padding(word, max_length) do
+    len = String.length(word)
+    div(max_length - len, 2) + len
+  end
+
+  defp parse_expression(expression) do
     regex = ~r/(?<num1>\d+)\s?(?<operator>[\*\/+-])\s?(?<num2>\d+)/
     Regex.run(regex, expression, capture: :all_but_first)
   end
