487e0e5b516e94d4787a491f65306d1a59426815
Sean Omlor <somlor@eml.cc>
2015-08-21 23:18:42 -0400

    revamp fizzbuzz as FizzBuzz.upto(n)

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,10 +1,19 @@
-fizzbuzz = fn
-  (0, 0, _) -> "fizzbuzz"
-  (0, _, _) -> "fizz"
-  (_, 0, _) -> "buzz"
-  (_, _, c) -> c
-end
+defmodule FizzBuzz do
+  def upto(final), do: upto(final, 0, [])
 
-fb = fn (n) ->
-  fizzbuzz.(rem(n, 3), rem(n, 5), n)
+  defp upto(final, current, result) when current >= final, do: result
+
+  defp upto(final, current, result) do
+    fizzbuzz = ""
+      |> concat_if("Fizz", rem(current, 3) == 0)
+      |> concat_if("Buzz", rem(current, 5) == 0)
+      |> current_if_empty(current)
+
+    upto(final, current + 1, result ++ [fizzbuzz])
+  end
+
+  defp concat_if(a, b, predicate), do: predicate && a <> b || a
+
+  defp current_if_empty("", current), do: current
+  defp current_if_empty(fizzbuzz, _), do: fizzbuzz
 end
