3ad3417c539be6a48a652ad6947f667223f55546
Sean Omlor <somlor@eml.cc>
2015-08-22 00:31:22 -0400

    case version of fizzbuzz
    plus keep cond and guard versions for comparison

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,9 +1,31 @@
 defmodule FizzBuzz do
 
-  def upto(final), do: 1..final |> Enum.map(&fizzbuzz/1)
+  def upto_case(final), do: 1..final |> Enum.map(&fizzbuzz_case/1)
 
-  def fizzbuzz(n) when rem(n, 3) == 0 and rem(n, 5) == 0, do: "FizzBuzz"
-  def fizzbuzz(n) when rem(n, 3) == 0, do: "Fizz"
-  def fizzbuzz(n) when rem(n, 5) == 0, do: "Fizz"
-  def fizzbuzz(n), do: n
+  def fizzbuzz_case(n) do
+    case {rem(n, 3), rem(n, 5)} do
+      {0, 0} -> "FizzBuzz"
+      {0, _} -> "Fizz"
+      {_, 0} -> "Buzz"
+      _      -> n
+    end
+  end
+
+  def upto_cond(final), do: 1..final |> Enum.map(&fizzbuzz_cond/1)
+
+  def fizzbuzz_cond(n) do
+    cond do
+      rem(n, 3) == 0 and rem(n, 5) == 0 -> "FizzBuzz"
+      rem(n, 3) == 0                    -> "Fizz"
+      rem(n, 5) == 0                    -> "Buzz"
+      true                              -> n
+    end
+  end
+
+  def upto_guard(final), do: 1..final |> Enum.map(&fizzbuzz_guard/1)
+
+  def fizzbuzz_guard(n) when rem(n, 3) == 0 and rem(n, 5) == 0, do: "FizzBuzz"
+  def fizzbuzz_guard(n) when rem(n, 3) == 0, do: "Fizz"
+  def fizzbuzz_guard(n) when rem(n, 5) == 0, do: "Fizz"
+  def fizzbuzz_guard(n), do: n
 end
