b76d84562f473842b8f0941cbea8688a518d1905
Sean Omlor <somlor@eml.cc>
2015-08-21 23:54:57 -0400

    fizzbuzz pattern matching for the win

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -2,15 +2,8 @@ defmodule FizzBuzz do
 
   def upto(final), do: 1..final |> Enum.map(&fizzbuzz/1)
 
-  def fizzbuzz(num) do
-    ""
-    |> concat_if("Fizz", rem(num, 3) == 0)
-    |> concat_if("Buzz", rem(num, 5) == 0)
-    |> num_if_empty(num)
-  end
-
-  defp concat_if(a, b, predicate), do: predicate && a <> b || a
-
-  defp num_if_empty("", num), do: num
-  defp num_if_empty(fizzbuzz, _), do: fizzbuzz
+  def fizzbuzz(n) when rem(n, 3) == 0 and rem(n, 5) == 0, do: "FizzBuzz"
+  def fizzbuzz(n) when rem(n, 3) == 0, do: "Fizz"
+  def fizzbuzz(n) when rem(n, 5) == 0, do: "Fizz"
+  def fizzbuzz(n), do: n
 end
