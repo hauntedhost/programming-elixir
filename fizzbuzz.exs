f2b7f5c08a389278cc6bbeeb2824416761a705a4
Sean Omlor <somlor@eml.cc>
2015-08-21 23:42:34 -0400

    use range for fizzbuzz
    much cleaner, no need for recursion

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,19 +1,16 @@
 defmodule FizzBuzz do
-  def upto(final), do: upto(final, 0, [])
 
-  defp upto(final, current, result) when current >= final, do: result
+  def upto(final), do: 1..final |> Enum.map(&fizzbuzz/1)
 
-  defp upto(final, current, result) do
-    fizzbuzz = ""
-      |> concat_if("Fizz", rem(current, 3) == 0)
-      |> concat_if("Buzz", rem(current, 5) == 0)
-      |> current_if_empty(current)
-
-    upto(final, current + 1, result ++ [fizzbuzz])
+  def fizzbuzz(num) do
+    ""
+    |> concat_if("Fizz", rem(num, 3) == 0)
+    |> concat_if("Buzz", rem(num, 5) == 0)
+    |> num_if_empty(num)
   end
 
   defp concat_if(a, b, predicate), do: predicate && a <> b || a
 
-  defp current_if_empty("", current), do: current
-  defp current_if_empty(fizzbuzz, _), do: fizzbuzz
+  defp num_if_empty("", num), do: num
+  defp num_if_empty(fizzbuzz, _), do: fizzbuzz
 end
