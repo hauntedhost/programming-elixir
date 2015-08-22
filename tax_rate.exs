318296a3eefcd6b9dd9069c900b563f7a9f7dd3b
Sean Omlor <somlor@eml.cc>
2015-08-21 22:22:02 -0400

    update tax_rate to stream from csv file

--- tax_rate.exs
+++ tax_rate.exs
@@ -1,28 +1,23 @@
 defmodule TaxRate do
-  def orders_with_totals do
-    orders |> Enum.map(&add_total/1)
-  end
 
-  def add_total(order = [ id: _, ship_to: state, net_amount: net]) do
+  def add_total(order = [_, state, net]) do
     tax_rate = Keyword.get(tax_rates, state, 0)
     tax      = net * tax_rate
     total    = net + tax
-    Keyword.put(order, :total_amount, total)
+    order ++ [total]
   end
 
-  # data
+  def orders_from_file(filename \\ "orders.csv") do
+    File.stream!(filename) |> Stream.map(&(parse_order_line(&1)))
+  end
 
-  def orders do
-    [
-      [ id: 123, ship_to: :NC, net_amount: 100.00 ],
-      [ id: 124, ship_to: :OK, net_amount:  35.50 ],
-      [ id: 125, ship_to: :TX, net_amount:  24.00 ],
-      [ id: 126, ship_to: :TX, net_amount:  44.80 ],
-      [ id: 127, ship_to: :NC, net_amount:  25.00 ],
-      [ id: 128, ship_to: :MA, net_amount:  10.00 ],
-      [ id: 129, ship_to: :CA, net_amount: 102.00 ],
-      [ id: 120, ship_to: :NC, net_amount:  50.00 ]
-    ]
+  def orders_with_totals(orders) do
+    orders |> Enum.map(&add_total/1)
+  end
+
+  defp parse_order_line(order) do
+    [id, state, net] = order |> String.rstrip(?\n) |> String.split(",")
+    [String.to_integer(id), String.to_atom(state), String.to_float(net)]
   end
 
   def tax_rates do
