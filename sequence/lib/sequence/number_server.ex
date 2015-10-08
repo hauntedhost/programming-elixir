0c011574a15e5262692cae84963d1ab9e0550b09
Sean Omlor <somlor@eml.cc>
2015-10-07 21:51:37 -0400

    hot swap number server ftw! ᕕ( ᐛ )ᕗ

--- sequence/lib/sequence/number_server.ex
+++ sequence/lib/sequence/number_server.ex
@@ -1,5 +1,8 @@
 defmodule Sequence.NumberServer do
   use GenServer
+  require Logger
+
+  @vsn "1"
 
   ## API
 
@@ -28,23 +31,31 @@ defmodule Sequence.NumberServer do
   ## GenServer
 
   def init(_) do
-    current_number = Sequence.Stash.get(:number) || 0
-    {:ok, current_number}
+    number = Sequence.Stash.get(:number) || 0
+    {:ok, %{number: number, delta: 1}}
   end
 
-  def handle_call(:next, _from, current_number) do
-    {:reply, current_number, current_number + 1}
+  def handle_call(:next, _from, state) do
+    {:reply, state.number, %{state | number: state.number + state.delta}}
   end
 
-  def handle_cast({:inc, delta}, current_number) do
-    {:noreply, current_number + delta}
+  def handle_cast({:inc, delta}, state) do
+    {:noreply, %{number: state.number + delta, delta: delta}}
   end
 
-  def handle_cast({:set, new_number}, _current_number) do
-    {:noreply, new_number}
+  def handle_cast({:set, new_number}, state) do
+    {:noreply, %{state | number: new_number}}
   end
 
   def terminate(_reason, current_number) do
     Sequence.Stash.put(:number, current_number)
+  end
+
+  def code_change("0", old_state, _extra) do
+    new_state = %{number: old_state, delta: 1}
+    Logger.info "Changing code from 0 to 1"
+    Logger.info inspect(old_state)
+    Logger.info inspect(new_state)
+    {:ok, new_state}
   end
 end
