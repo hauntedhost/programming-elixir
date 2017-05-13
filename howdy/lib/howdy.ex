11af0abca198cb6bfa237ebb7930d4c9408f69a8
Sean Omlor <somlor@eml.cc>
2017-05-13 14:26:34 -0400

    new mix supervisor app

---
+++ howdy/lib/howdy.ex
@@ -0,0 +1,18 @@
+defmodule Howdy do
+  @moduledoc """
+  Documentation for Howdy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Howdy.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ howdy/test/howdy_test.exs
@@ -0,0 +1,8 @@
+defmodule HowdyTest do
+  use ExUnit.Case
+  doctest Howdy
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
